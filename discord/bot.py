de009540a492d354668f74ac73e50a8ab9d763bf
Ria <75088570+Rishabh4Jakhar@users.noreply.github.com>
2021-12-23 20:42:42 +0530

    Update bot.py

--- discord/bot.py
+++ discord/bot.py
@@ -956,7 +956,7 @@ class BotBase(ApplicationCommandMixin, CogMixin):
         await super().dispatch(event_name, *args, **kwargs)  # type: ignore
         ev = 'on_' + event_name
         for event in self.extra_events.get(ev, []):
-            await self._schedule_event(event, ev, *args, **kwargs)  # type: ignore
+            self._schedule_event(event, ev, *args, **kwargs)  # type: ignore
 
     def before_invoke(self, coro):
         """A decorator that registers a coroutine as a pre-invoke hook.
